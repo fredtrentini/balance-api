40bbe96c1089bf21874ccfa5a1cbd68f9fd121ce
Fred <fredtrentini@gmail.com>
2025-12-10 13:00:50 -0300

    Minimalistic GET /balance

--- src/routes/get-balance.ts
+++ src/routes/get-balance.ts
@@ -1,11 +1,26 @@
 import express, { type Request, type Response } from "express";
 
 const router = express.Router();
+const accountIdToBalanceMap: Record<number, number> = {};
 
 router.get(
     "/balance",
     async (req: Request, res: Response) => {
-        res.send({});
+        const accountId: number = Number(req.query.account_id);
+
+        if (Number.isNaN(accountId)) {
+            return res.status(400).json({
+                error: "Missing account_id query param"
+            });
+        }
+        
+        const balance = accountIdToBalanceMap[accountId] ?? null;
+
+        if (balance === null) {
+            return res.status(404).json(0);
+        }
+
+        res.send(balance);
     }
 );
 
