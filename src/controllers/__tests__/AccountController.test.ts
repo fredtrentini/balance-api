50633cbcf31e4a3d0b6df8c657331810a531d708
Fred <fredtrentini@gmail.com>
2025-12-10 22:39:26 -0300

    POST /event integration tests
    Applying TDD from documented use cases before setting up controller logic

--- src/controllers/__tests__/AccountController.test.ts
+++ src/controllers/__tests__/AccountController.test.ts
@@ -65,6 +65,161 @@ describe("GET /balance", () => {
     });
 });
 
+describe("POST /event", () => {
+    it("validates event type", async () => {
+        const accountId = "123";
+        const event = { type: "invalid_event_type", destination: accountId, amount: 10 };
+
+        const response = await request(app)
+            .post("/event")
+            .send(event)
+            .expect(400);
+
+        expect(response.body.error).toBe("Invalid event type");
+    });
+
+    it("validates event amount", async () => {
+        const accountId = "123";
+        const event = { type: "deposit", destination: accountId, amount: -1 };
+
+        const response = await request(app)
+            .post("/event")
+            .send(event)
+            .expect(400);
+
+        expect(response.body.error).toBe("Invalid event amount");
+    });
+
+    it("creates account with initial balance", async () => {
+        const accountId = "123";
+        const event = { type: "deposit", destination: accountId, amount: 10 };
+
+        const response = await request(app)
+            .post("/event")
+            .send(event)
+            .expect(201);
+
+        expect(response.body).toBe({ destination: { id: accountId, balance: 10 } });
+    });
+
+    it("deposits into existing account", async () => {
+        const accountId = "123";
+        const event = { type: "deposit", destination: accountId, amount: 10 };
+
+        await request(app)
+            .post("/event")
+            .send(event)
+            .expect(201);
+
+        const response = await request(app)
+            .post("/event")
+            .send(event)
+            .expect(201);
+
+        expect(response.body).toBe({ destination: { id: accountId, balance: 20 } });
+    });
+
+    it("withdraws from unexistent account", async () => {
+        const origin = "123";
+        const event = { type: "withdraw", origin, amount: 10 };
+
+        const response = await request(app)
+            .post("/event")
+            .send(event)
+            .expect(404);
+
+        expect(response.body).toBe(0);
+    });
+
+    it("withdraws from existing account", async () => {
+        const origin = "123";
+        const event = { type: "withdraw", origin, amount: 10 };
+        const balance = 100;
+
+        await accountService.setBalance(origin, balance);
+
+        const response = await request(app)
+            .post("/event")
+            .send(event)
+            .expect(201);
+
+        expect(response.body).toBe({ origin: { id: origin, balance: 90 } });
+    });
+
+    it("withdraws amount greater than balance", async () => {
+        const origin = "123";
+        const event = { type: "withdraw", origin, amount: 101 };
+        const balance = 100;
+
+        await accountService.setBalance(origin, balance);
+
+        const response = await request(app)
+            .post("/event")
+            .send(event)
+            .expect(400);
+
+        expect(response.body.error).toBe("Amount must not be greater than balance");
+    });
+
+    it("validates transfer without destination", async () => {
+        const origin = "123";
+        const event = { type: "transfer", origin, amount: 10 };
+
+        const response = await request(app)
+            .post("/event")
+            .send(event)
+            .expect(400);
+
+        expect(response.body.error).toBe("Transfer requires a destination");
+    });
+
+    it("transfers from existing account", async () => {
+        const origin = "123";
+        const destination = "321";
+        const event = { type: "transfer", origin, amount: 10, destination };
+
+        await accountService.setBalance(origin, 100);
+        await accountService.setBalance(destination, 50);
+
+        const response = await request(app)
+            .post("/event")
+            .send(event)
+            .expect(201);
+
+        expect(response.body).toBe({ origin: { id: origin, balance: 90 }, destination: { id: destination, balance: 60 } });
+    });
+
+    it("transfers from unexistent account", async () => {
+        const origin = "123";
+        const destination = "321";
+        const event = { type: "transfer", origin, amount: 10, destination };
+
+        await accountService.setBalance(destination, 100);
+
+        const response = await request(app)
+            .post("/event")
+            .send(event)
+            .expect(404);
+
+        expect(response.body).toBe(0);
+    });
+
+    it("transfers to unexistent account", async () => {
+        const origin = "123";
+        const destination = "321";
+        const event = { type: "transfer", origin, amount: 10, destination };
+
+        await accountService.setBalance(origin, 100);
+
+        const response = await request(app)
+            .post("/event")
+            .send(event)
+            .expect(404);
+
+        expect(response.body).toBe(0);
+    });
+});
+
 describe("POST /reset", () => {
     it("resets state", async () => {
         const accountId = "123";
